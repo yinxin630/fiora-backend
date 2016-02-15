6943c5d7c01a0efde3c169075086ab0dbca3013a
碎碎酱 <yinxin630@gmail.com>
2016-02-15 22:42:25 +0800

    feat(db): config database. use local mongoDB

--- config/connections.js
+++ config/connections.js
@@ -56,7 +56,7 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  mongoDB: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
--- config/models.js
+++ config/models.js
@@ -17,7 +17,7 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  // connection: 'localDiskDb',
+  connection: 'mongoDB',
 
   /***************************************************************************
   *                                                                          *
@@ -27,6 +27,6 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  // migrate: 'alter'
+  migrate: 'alter'
 
 };
