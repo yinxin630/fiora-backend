e7d55680811db410d88d0177a80101b292abf76c
碎碎酱 <yinxin630@gmail.com>
2016-02-27 09:38:36 +0800

    feat(guest): add guest model

---
+++ api/controllers/GuestController.js
@@ -0,0 +1,13 @@
+/**
+ * GuestController
+ *
+ * @description :: Server-side logic for managing guests
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	create: function (req, res) {
+        
+    }
+};
+
