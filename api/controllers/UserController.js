6fd518327812b9567ef6664b6111e1cf70451a0a
碎碎酱 <yinxin630@gmail.com>
2016-02-27 09:37:19 +0800

    feat(user): add user model

---
+++ api/controllers/UserController.js
@@ -0,0 +1,13 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	create: function(req, res) {
+        
+    }
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,20 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+    attributes: {
+        username: {
+            type: 'string',
+        },
+        
+        password: {
+            type: 'string',
+        }
+    }
+};
+
