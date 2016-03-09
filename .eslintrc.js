d8a5a11cbbff86ecb4bd83da62accfd4ebe86e03
碎碎酱 <yinxin630@gmail.com>
2016-03-09 10:21:02 +0800

    feat(eslint): add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            2,
+            4
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    }
+};
